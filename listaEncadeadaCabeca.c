5e2a77f2c7cc58b75cc5d4fdada7314d015ca690
Marcus Paulo <mlima.paulo9@gmail.com>
2017-09-26 00:20:37 -0300

    Implementacao do Menu da Lista encadeada

--- listaEncadeadaCabeca.c
+++ listaEncadeadaCabeca.c
@@ -10,17 +10,74 @@ typedef struct sElemento
     struct sELemento* pProx;
 }tElemento;
 
-tElemento* icializarListaCabeca();
+tElemento* inicializarListaCabeca();
 
 int main()
 {
+    int opcao;
 
+    tElemento* l1 = inicializarListaCabeca;
 
+    do{
+
+        printf("\n");
+		printf("MENU: Lista simplesmente encadeada \n");
+        printf("1-Percorrer \n");
+        printf("2-Proximo ID (na cabeca) \n");
+        printf("3-Buscar Elemento Atual \n");
+        printf("4-Buscar Elemento Anterior \n");
+        printf("5-Inserir no Inicio \n");
+        printf("6-Inserir no Fim \n");
+        printf("7-Inserir (depois de...) \n");
+        printf("8-Remover \n");
+        printf("0-Sair \n");
+        printf("Qual sua opcao? ");
+        scanf("%d", &opcao);
+        printf("\n");
+            
+        switch(opcao)
+        {
+            case 1:printf("Percorrer\n");
+                   break;
+                
+            case 2: printf("PROXIMO ID (na cabeca) \n");
+                   break;
+
+            case 3: printf("BUSCAR NORMAL \n");
+                   break;
+
+            case 4: printf("BUSCAR ANTERIOR \n");
+                   break;
+
+            case 5: printf("INSERIR INICIO \n");
+                   break;
+
+            case 6: printf("INSERIR FIM \n");
+                   break;
+
+            case 7: printf("INSERIR DEPOIS DE... \n");
+                   break;
+
+            case 8: printf("REMOVER \n");
+                   break;
+
+            case 0: printf("Saindo... \n");
+                   break;
+
+            default: printf("OPCAO INVALIDA \n");
+                    break;
+        }
+        
+
+        printf("\n\n");
+        system("clear");        
+    }
+    while(opcao != 0);
     return 0;
 
 }
 
-tElemento* icializarListaCabeca()
+tElemento* inicializarListaCabeca()
 {
 
     tElemento* lista = (tElemento*) calloc(1,sizeof(tElemento));
